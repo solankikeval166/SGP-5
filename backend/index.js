1429735d1ec0b70e03f94ca8901a0e6f6adba508
solankikeval166 <solankikeval166@gmail.com>
2023-09-05 07:18:52 +0530

    First commit

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+const port = 3000;
+app.listen(port, function () {
+  console.log("Server is listening on " + port);
+});
